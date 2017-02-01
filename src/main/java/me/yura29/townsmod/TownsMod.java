58b558926fc4442184209724fd916c10a6b2df8a
Anonim <Anonim>
2017-02-01 22:53:47 +0400

    Mod structuring and adding a creative tab

--- src/main/java/me/yura29/townsmod/TownsMod.java
+++ src/main/java/me/yura29/townsmod/TownsMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package me.yura29.townsmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
---
+++ src/main/java/me/yura29/townsmod/proxy/CommonProxy.java
@@ -0,0 +1,7 @@
+package me.yura29.townsmod.proxy;
+
+/**
+ * Created by Anonim on 01.02.2017.
+ */
+public class CommonProxy {
+}
